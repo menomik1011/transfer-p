72e68698166e793dc06ac7b5a1010bd40cd43053
menomik1011 <meno.mik1011@gmail.com>
2023-01-09 18:04:17 +0900

    first commit

---
+++ pages/login.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const LoginBlock = styled.div`
+    width: 350px;
+    border: 1px solid #dbdbdb;
+    background-color: #fff;
+`
+
+export default function Login(){
+    return <LoginBlock>login</LoginBlock>
+}

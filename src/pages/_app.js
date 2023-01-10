d1ed4aa19d5affad4615fd51c7664050f8e9b1d4
menomik1011 <meno.mik1011@gmail.com>
2023-01-10 17:15:36 +0900

    add src folder

---
+++ src/pages/_app.js
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router";
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  console.log(router.pathname);
+  return (
+    <div className="container">
+      <Component {...pageProps} />
+    </div>
+  );
+}
